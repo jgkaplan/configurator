a9c47c6c935b880e97350f294a210efbf26fb33a
Joshua Kaplan <joshuagraykaplan@gmail.com>
2023-03-29 03:23:15 -0500

    Working parsing code

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::cmp::PartialEq;
+use std::mem::discriminant;
 
 pub type Ident = String;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Binop {
     Eq,
     Neq,
@@ -21,32 +24,182 @@ pub enum Binop {
     Pow
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Unop {
     Neg,
     Not
 }
 
+#[derive(Eq, Clone, Debug)]
+pub enum Type {
+    Null,
+    List(Box<Type>),
+    Natural,
+    Integer,
+    Real,
+    Number,
+    Function(Box<Type>, Box<Type>),
+    Bool,
+    Record(HashMap<Ident, Type>),
+    Text,
+    Alternative(Box<Type>, Box<Type>),
+    Any,
+    Type,
+    Ident(Ident)
+}
+
+/*
+Any > other
+List[Number] > List[Integer]
+Number > Integer
+if expecting a, can use b with a > b
+ */
+impl PartialOrd for Type {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        use Ordering::*;
+        match (self, other) {
+            //any
+            (Type::Any, Type::Any) => Some(Equal),
+            (_, Type::Any) => Some(Less),
+            (Type::Any, _) => Some(Greater),
+            //numbers
+            (Type::Number, Type::Natural) | (Type::Number, Type::Real) | (Type::Number, Type::Integer) => Some(Greater),
+            (Type::Natural, Type::Number) | (Type::Real, Type::Number) | (Type::Integer, Type::Number) => Some(Less),
+            (Type::Integer, Type::Natural) | (Type::Real, Type::Integer) | (Type::Real, Type::Natural) => Some(Greater),
+            (Type::Natural, Type::Integer) | (Type::Integer, Type::Real) | (Type::Natural, Type::Real) => Some(Less),
+            //list
+            (Type::List(t1), Type::List(t2)) => (*t1).partial_cmp(&*t2),
+            //record
+            (Type::Record(map1), Type::Record(map2)) if map1 == map2 => Some(Equal),
+            (Type::Record(map1), Type::Record(map2)) => {
+                let smaller;
+                let larger;
+                let result;
+                if map1.len() < map2.len() {
+                    smaller = map1;
+                    larger = map2;
+                    result = Some(Greater);
+                } else {
+                    smaller = map2;
+                    larger = map1;
+                    result = Some(Less);
+                }
+                let large_has_small = smaller.iter().all(|(key, val)| larger.get(key).map_or(false, |v| val == v));
+                if large_has_small {
+                    result
+                } else{
+                    None
+                }
+            }
+            //alternative: todo
+            (t, Type::Alternative(t1, t2)) if t <= &*t1  || t <= &*t2 => Some(Less),
+            (t, Type::Alternative(t1, t2)) if t >= &*t1 && t >= &*t2 => Some(Greater),
+            (Type::Alternative(t1, t2), t) if t <= &*t1  || t <= &*t2 => Some(Greater),
+            (Type::Alternative(t1, t2), t) if t >= &*t1  && t >= &*t2 => Some(Less),
+            (Type::Alternative(t1,t2), Type::Alternative(t3,t4)) => {
+                let c13 = (*t1).partial_cmp(&*t3);
+                let c14 = (*t1).partial_cmp(&*t4);
+                let c23 = (*t2).partial_cmp(&*t3);
+                let c24 = (*t2).partial_cmp(&*t4);
+                // TODO: I think this is wrong
+                match (c13, c14, c23, c24) {
+                    (Some(x), _, _, Some(y)) if x == y => Some(x),
+                    (_, Some(x), Some(y),_) if x == y => Some(x),
+                    _ => None
+                }
+                // t1 | t2 < t3 | t4
+                // Number | Bool 
+            },
+            //unknown: todo
+            //equal type
+            (t1, t2) if t1 == t2 => Some(Ordering::Equal),
+            _ => None
+        }
+    }
+}
+
+impl PartialEq for Type {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Type::Alternative(a,b), Type::Alternative(c,d)) => {
+                *a == *c && *b == *d || *a == *d && *b == *c
+            },
+            (Type::Alternative(a,b), t) => *t == **a && *t == **b,
+            (t, Type::Alternative(a,b)) => *t == **a && *t == **b,
+            (Type::List(a), Type::List(b)) => **a == **b,
+            (Type::Function(a,b), Type::Function(c,d)) => **a == **c && **b == **d,
+            (Type::Record(a), Type::Record(b)) => {
+                a == b
+                // if self.len() != other.len() {
+                //     return false;
+                // }
+                // for (key, val) in a {
+                //     let v = b.get(key);
+                //     match v {
+                //         None => return false,
+                //         Some(v1) => {
+                //             if *val != *v1{
+                //                 return false;
+                //             }
+                //         }
+                //     }
+                // }
+                // a.iter().all(|(key, val)| other.get(key).map_or(false, |v| *val == *v));
+                // true
+            },
+            (t1, t2) => discriminant(t1) == discriminant(t2)
+        }
+    }
+}
+
 #[derive(Debug)]
-pub enum Expr {
-    Let(Ident, Box<Expr>, Box<Expr>),
-    If(Box<Expr>, Box<Expr>, Box<Expr>),
-    App(Box<Expr>, Box<Expr>),
-    Binop(Box<Expr>, Binop, Box<Expr>),
-    Unop(Unop, Box<Expr>),
+pub struct Expr {
+    pub t: Option<Type>,
+    pub expr: ExprKind<Self>
+}
+
+#[derive(Debug)]
+pub struct TypedExpr {
+    pub t: Type,
+    pub expr: ExprKind<Self>
+}
+
+
+#[derive(Debug)]
+pub enum ExprKind<Wrapper> {
+    Let(Ident, Box<Wrapper>, Box<Wrapper>),
+    If(Box<Wrapper>, Box<Wrapper>, Box<Wrapper>),
+    App(Box<Wrapper>, Box<Wrapper>),
+    Binop(Box<Wrapper>, Binop, Box<Wrapper>),
+    Unop(Unop, Box<Wrapper>),
     Ident(Ident),
-    Record(HashMap<Ident, Box<Expr>>),
-    List(Vec<Expr>),
+    Record(HashMap<Ident, Wrapper>),
+    List(Vec<Wrapper>),
     Text(String),
     Number(f64),
     Boolean(bool),
-    Lambda(Ident,Box<Expr>),
+    Lambda(Ident,Box<Wrapper>),
     Null,
     //Color, Version, Path
 }
 
-// pub enum Value {
-    
+
+// #[derive(Debug)]
+// pub struct Value {
+//     pub t: Type,
+//     pub val: ValueKind
+// }
+
+// #[derive(Debug)]
+// pub enum ValueKind {
+//     Record(HashMap<Ident, Value>),
+//     List(Vec<Value>),
+//     Text(String),
+//     Number(f64),
+//     Boolean(bool),
+//     Lambda(Ident,Expr),
+//     Null,
+//     //Color, Version, Path
 // }
 
 // do we consider a function to be a value
--- src/parser.rs
+++ src/parser.rs
@@ -2,7 +2,8 @@ use pest::Parser;
 use std::collections::HashMap;
 use pest::iterators::Pair;
 use crate::ast::Expr;
-use crate::ast::Expr::*;
+use crate::ast::ExprKind::*;
+use crate::ast::Type;
 use crate::ast::Ident;
 use crate::ast::Binop;
 use crate::ast::Unop;
@@ -26,46 +27,59 @@ fn parse_ident(pair: Pair<Rule>) -> Ident {
     pair.as_str().to_string()
 }
 
-fn parse_term(pair: Pair<Rule>) -> Expr {
+fn parse_type(pair: Pair<Rule>) -> Type {
     match pair.as_rule() {
-        Rule::list => {
-            List(pair.into_inner().map(parse_expr).collect())
+        Rule::function_type => {
+            let mut it = pair.into_inner();
+            let t1 = parse_type(it.next().unwrap());
+            let t2 = parse_type(it.next().unwrap());
+            Type::Function(Box::new(t1), Box::new(t2))
         },
-        Rule::record => {
+        Rule::list_type => {
+            let t = parse_type(pair.into_inner().next().unwrap());
+            Type::List(Box::new(t))
+        },
+        Rule::record_type => {
             let i = pair.into_inner();
-            let mut hashmap: HashMap<Ident, Box<Expr>>= HashMap::new();
+            let mut hashmap: HashMap<Ident, Type>= HashMap::new();
             for record_pair in i {
                 let mut inner_rules = record_pair.into_inner();
                 let k = parse_ident(inner_rules.next().unwrap());
-                let v = parse_expr(inner_rules.next().unwrap());
-                hashmap.insert(k, Box::new(v));
+                let v = parse_type(inner_rules.next().unwrap());
+                hashmap.insert(k, v);
             }
-            Record(hashmap)
+            Type::Record(hashmap)
         },
-        Rule::string => {
-            Text(pair.into_inner().next().unwrap().as_str().to_string())
-        },
-        // Rule::version,
-        Rule::number => {
-            Number(pair.as_str().parse().unwrap())
-        },
-        Rule::bool => {
-            Boolean(pair.as_str().parse().unwrap())
-        },
-        // Rule::color,
-        Rule::null => Null,
-        Rule::ident => {
-            Ident(parse_ident(pair))
-        },
-        Rule::lambda => {
+        Rule::alternative_type => {
             let mut it = pair.into_inner();
-            let x = parse_ident(it.next().unwrap());
-            let e = parse_expr(it.next().unwrap());
-            Lambda(x, Box::new(e))
+            let t1 = parse_type(it.next().unwrap());
+            let t2 = parse_type(it.next().unwrap());
+            Type::Alternative(Box::new(t1), Box::new(t2))
         },
-        Rule::paren_expr => {
-            parse_expr(pair.into_inner().next().unwrap())
-        }
+        Rule::user_type => {
+            let id = parse_ident(pair.into_inner().next().unwrap());
+            Type::Ident(id)
+        },
+        Rule::builtin_type => {
+            match pair.into_inner().next().unwrap().as_str() {
+                "Bool" => Type::Bool,
+                "Text" => Type::Text,
+                "Number" => Type::Number,
+                "Natural" => Type::Natural,
+                "Integer" => Type::Integer,
+                "Real" => Type::Real,
+                // "Color" => Type::Color,
+                // "Path" => Type::Path,
+                "Null" => Type::Null,
+                // "Version" => Type::Version, // semver
+                "Any" => Type::Any,
+                "Type" => Type::Type,
+                _ => unreachable!()
+            }
+        },
+        Rule::paren_type => {
+            parse_type(pair.into_inner().next().unwrap())
+        },
         _ => unreachable!()
     }
 }
@@ -77,33 +91,43 @@ fn parse_expr(pair: Pair<Rule>) -> Expr {
             let ident = it.next().unwrap();
             let ident: Ident = ident.to_string();
             let e1 = it.next().unwrap();
-            let e1 = parse_term(e1);
+            let e1 = parse_expr(e1);
             let e2 = it.next().unwrap();
             let e2 = parse_expr(e2);
-            Let(ident, Box::new(e1), Box::new(e2))
+            Expr {
+                t: None,
+                expr: Let(ident, Box::new(e1), Box::new(e2))
+            }
+            
         }
         Rule::if_expr => {
             let mut it = pair.into_inner();
             let b = it.next().unwrap();
-            let b = parse_term(b);
+            let b = parse_expr(b);
             let e1 = it.next().unwrap();
-            let e1 = parse_term(e1);
+            let e1 = parse_expr(e1);
             let e2 = it.next().unwrap();
-            let e2 = parse_term(e2);
-            If(Box::new(b), Box::new(e1), Box::new(e2))
+            let e2 = parse_expr(e2);
+            Expr {
+                t: None,
+                expr: If(Box::new(b), Box::new(e1), Box::new(e2))
+            }
         },
         Rule::function_application => {
             let mut it = pair.into_inner();
             let e1 = it.next().unwrap();
-            let e1 = parse_term(e1);
+            let e1 = parse_expr(e1);
             let e2 = it.next().unwrap();
-            let e2 = parse_term(e2);
-            App(Box::new(e1), Box::new(e2))
+            let e2 = parse_expr(e2);
+            Expr {
+                t: None,
+                expr: App(Box::new(e1), Box::new(e2))
+            }
         },
         Rule::binop_expr => {
             let mut it = pair.into_inner();
             let e1 = it.next().unwrap();
-            let e1 = parse_term(e1);
+            let e1 = parse_expr(e1);
             let bop = it.next().unwrap();
             let bop: Binop = match bop.as_str() {
                 "+" => Binop::Plus,
@@ -124,8 +148,11 @@ fn parse_expr(pair: Pair<Rule>) -> Expr {
                 _ => unreachable!()
             };
             let e2 = it.next().unwrap();
-            let e2 = parse_term(e2);
-            Binop(Box::new(e1), bop, Box::new(e2))
+            let e2 = parse_expr(e2);
+            Expr {
+                t: None,
+                expr: Binop(Box::new(e1), bop, Box::new(e2))
+            }
         },
         Rule::unop_expr => {
             let mut it = pair.into_inner();
@@ -136,12 +163,87 @@ fn parse_expr(pair: Pair<Rule>) -> Expr {
                 _ => unreachable!()
             };
             let e = it.next().unwrap();
-            let e = parse_term(e);
-            Unop(unop, Box::new(e))
+            let e = parse_expr(e);
+            Expr {
+                t: None,
+                expr: Unop(unop, Box::new(e))
+            }
         },
-        Rule::term => parse_term(pair),
+        Rule::term => parse_expr(pair),
+        Rule::list => {
+            Expr {
+                t: None,
+                expr: List(pair.into_inner().map(parse_expr).collect())
+            }
+        },
+        Rule::record => {
+            let i = pair.into_inner();
+            let mut hashmap: HashMap<Ident, Expr>= HashMap::new();
+            for record_pair in i {
+                let mut inner_rules = record_pair.into_inner();
+                let k = parse_ident(inner_rules.next().unwrap());
+                let v = parse_expr(inner_rules.next().unwrap());
+                hashmap.insert(k, v);
+            }
+            Expr {
+                t: None,
+                expr: Record(hashmap)
+            }
+        },
+        Rule::string => Expr {
+            t: Some(Type::Text),
+            expr: Text(pair.into_inner().next().unwrap().as_str().to_string())
+        },
+        // Rule::version,
+        //TODO: maybe split this parsing so that we can get the type better
+        Rule::number => Expr {
+            t: Some(Type::Number),
+            expr: Number(pair.as_str().parse().unwrap())
+        },
+        Rule::bool => Expr {
+            t: Some(Type::Bool),
+            expr: Boolean(pair.as_str().parse().unwrap())
+        },
+        // Rule::color,
+        Rule::null => Expr {
+            t: Some(Type::Null),
+            expr: Null
+        },
+        Rule::ident => Expr {
+            t: None,
+            expr: Ident(parse_ident(pair))
+        },
+        Rule::lambda => {
+            let lam = pair.into_inner().next().unwrap();
+            match lam.as_rule() {
+                // TODO: these rules don't do anything
+                Rule::untyped_lambda => {
+                    let mut it = lam.into_inner();
+                    let x = parse_ident(it.next().unwrap());
+                    let e = parse_expr(it.next().unwrap());
+                    Expr {
+                        t: None,
+                        expr: Lambda(x, Box::new(e))
+                    }
+                },
+                Rule::typed_lambda => {
+                    let mut it = lam.into_inner();
+                    let x = parse_ident(it.next().unwrap());
+                    let t = parse_type(it.next().unwrap());
+                    let e = parse_expr(it.next().unwrap());
+                    Expr {
+                        t: None,
+                        expr: Lambda(x, Box::new(e))
+                    }
+                },
+                _ => unreachable!()
+            }
+        },
+        Rule::paren_expr => {
+            parse_expr(pair.into_inner().next().unwrap())
+        },
         _ => {
-            println!("{:#?}", pair);
+            println!("{:#?}", pair); //TODO: remove this
             unreachable!()
         }
     }
